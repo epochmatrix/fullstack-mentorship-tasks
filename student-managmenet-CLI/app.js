e38822b74dc8dd33d86559f8461cd546601d466d
epochmatrix <abdulmejidreyan@gmail.com>
2026-07-22 09:14:37 +0300

    Initialize student database array n functoin skeletons

---
+++ student-managmenet-CLI/app.js
@@ -0,0 +1,32 @@
+//Student Management App
+
+let students = []; // Array to store student records
+
+// Function to add a new student
+const addStudent = (id, name, DOB, email, department) => {
+   
+};
+
+// view list students 
+const listStudents = () => {
+    
+};
+
+// serach student by id 
+const searchStudent =(id) => {
+
+}; 
+
+//update student
+const updateStudent = (id, updatedData) => {
+    
+};
+
+//delete student
+const deleteStudent = (id) => {
+
+}; 
+
+
+console.log("Student Managment System Initialized");
+console.log("current students:", students);

240e4c5aa97ca0a2c9638d6dc053750ecc7c274e
epochmatrix <abdulmejidreyan@gmail.com>
2026-07-22 09:40:36 +0300

    Implement addStudent function with ES6 syntax

--- student-managmenet-CLI/app.js
+++ student-managmenet-CLI/app.js
@@ -4,7 +4,9 @@ let students = []; // Array to store student records
 
 // Function to add a new student
 const addStudent = (id, name, DOB, email, department) => {
-   
+    const newStudent = {id, name, DOB, email, department};
+    students.push(newStudent);
+    console.log(`Student "${name}" (ID: ${id}) added successfully.`);
 };
 
 // view list students 
@@ -30,3 +32,8 @@ const deleteStudent = (id) => {
 
 console.log("Student Managment System Initialized");
 console.log("current students:", students);
+addStudent(1, "Reyan Abdulmejid", "2002-11-29", "weellyran@gmail.com", "Computer Science");
+addStudent(2, "Wildan Abdulmejid", "2002-11-29", "wildan@gmail.com", "Computer Science");
+addStudent(3, "Ferhan Reyan", "2002-11-29", "ferhan@gmail.com", "Computer Science");
+
+console.log("Updated Database:", students);
